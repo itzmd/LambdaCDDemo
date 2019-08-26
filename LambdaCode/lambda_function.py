a5f2ee02061c487149ea1c9a43b6135623ab20d3
itzmd <42710017+itzmd@users.noreply.github.com>
2019-08-26 11:25:59 +0530

    code change

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,7 +1,12 @@
-import boto3
-import json
-
-ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(response)}
+    print("In lambda handler")
+
+    resp = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": "Hello World"
+    }
+
+    return resp
